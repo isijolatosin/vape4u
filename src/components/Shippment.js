98512c7edfd374cce1f984cd97f0829538bdad2a
isijolatosin <isijolatosin2210@gmail.com>
2022-04-13 16:21:10 -0400

    resovling management error

--- src/components/Shippment.js
+++ src/components/Shippment.js
@@ -159,158 +159,158 @@ function Shippment() {
 	return (
 		<div className="flex flex-col items-center my-5">
 			<Heading>Shipment</Heading>
-			{user?.email === AUTHORIZED_ID.id_one || AUTHORIZED_ID.id_two ? (
-				<div>
-					{trackingInput && (
-						<div className="mt-10 w-full">
-							<input
-								type="text"
-								name="number"
-								id="number"
-								value={trackingNum.number}
-								onChange={handleChangeAuthUser}
-								placeholder="Tracking Number"
-								className="mt-1 block lg:w-[30%] mx-auto w-[70%] px-3 py-2 border border-neutral-100 text-sm shadow-xl placeholder-gray-400 focus:outline-none focus:border-blue-600 focus:ring-0 focus:ring-blue-600 isabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 outline-0 mb-5"
-							/>
-							<input
-								type="text"
-								name="courier"
-								id="courier"
-								value={trackingNum.courier}
-								onChange={handleChangeAuthUser}
-								placeholder="Courier Name"
-								className="mt-1 block lg:w-[30%] mx-auto w-[70%] px-3 py-2 border border-neutral-100 text-sm shadow-xl placeholder-gray-400 focus:outline-none focus:border-blue-600 focus:ring-0 focus:ring-blue-600 isabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 outline-0 mb-5"
-							/>
-							<div
-								className={
+			{/* {user?.email === AUTHORIZED_ID.id_one || AUTHORIZED_ID.id_two ? ( */}
+			<div>
+				{trackingInput && (
+					<div className="mt-10 w-full">
+						<input
+							type="text"
+							name="number"
+							id="number"
+							value={trackingNum.number}
+							onChange={handleChangeAuthUser}
+							placeholder="Tracking Number"
+							className="mt-1 block lg:w-[30%] mx-auto w-[70%] px-3 py-2 border border-neutral-100 text-sm shadow-xl placeholder-gray-400 focus:outline-none focus:border-blue-600 focus:ring-0 focus:ring-blue-600 isabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 outline-0 mb-5"
+						/>
+						<input
+							type="text"
+							name="courier"
+							id="courier"
+							value={trackingNum.courier}
+							onChange={handleChangeAuthUser}
+							placeholder="Courier Name"
+							className="mt-1 block lg:w-[30%] mx-auto w-[70%] px-3 py-2 border border-neutral-100 text-sm shadow-xl placeholder-gray-400 focus:outline-none focus:border-blue-600 focus:ring-0 focus:ring-blue-600 isabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 outline-0 mb-5"
+						/>
+						<div
+							className={
+								trackingNum.number === '' || trackingNum.courier === ''
+									? 'flex flex-col justify-center text-sm text-gray-200'
+									: 'flex flex-col justify-center text-sm text-green-700 hover:text-gray-400'
+							}>
+							<button
+								disabled={
 									trackingNum.number === '' || trackingNum.courier === ''
-										? 'flex flex-col justify-center text-sm text-gray-200'
-										: 'flex flex-col justify-center text-sm text-green-700 hover:text-gray-400'
-								}>
-								<button
-									disabled={
-										trackingNum.number === '' || trackingNum.courier === ''
-									}
-									onClick={() => hideShipped(_id)}>
-									Submit Tracking
-								</button>
-							</div>
+								}
+								onClick={() => hideShipped(_id)}>
+								Submit Tracking
+							</button>
 						</div>
-					)}
-					<main className="history-page">
-						{shippingInfo?.shippingHeader?.length === 0 &&
-						shippingInfo?.shippingHeader?.length === 0 ? (
-							isLoading ? (
-								<span>Loading...</span>
-							) : (
-								<span>Data not Found</span>
-							)
+					</div>
+				)}
+				<main className="history-page">
+					{shippingInfo?.shippingHeader?.length === 0 &&
+					shippingInfo?.shippingHeader?.length === 0 ? (
+						isLoading ? (
+							<span>Loading...</span>
 						) : (
-							<>
-								<div className="max-w-[90%] mr-[50px] md:px-[50px] md:max-w-[80%] lg:max-w-[100%] md:overflow-x-auto">
-									<table>
-										<thead>
-											<tr className="table-head-row">
-												{shippingInfo?.shippingHeader?.[0]?.map((head, idx) => (
-													<th key={idx} className="table-head-item">
-														{head}{' '}
-														{head === 'appointment' || head === 'reservation'
-															? ' - date'
-															: ''}
-													</th>
-												))}
-											</tr>
-										</thead>
-										<tbody>
-											{shippingInfo?.shippingData?.map((item, idx) => (
-												<tr className="table-item-row" key={idx}>
-													{Object?.values(item)?.map((itm, index) => (
-														<td
-															className={
-																itm?.length >= 200
-																	? 'table-items align-left'
-																	: 'table-items'
-															}
-															key={index}
-															onClick={() =>
-																toggleModal(item?.id, item?.customer)
-															}>
-															{typeof itm === 'string'
-																? itm?.length >= 200
-																	? readMore && matchIdx === item?.id
-																		? itm
-																		: `${itm.substring(0, 70)}...`
-																	: itm
-																: itm}
-															{itm?.length >= 200 && (
-																<>
-																	<span onClick={() => toggleRead(item?.id)}>
-																		{readMore && matchIdx === item?.id ? (
-																			<>
-																				<span>Read Less</span>{' '}
-																				<MdOutlineKeyboardArrowUp />
-																			</>
-																		) : (
-																			<>
-																				<span>Read More</span>{' '}
-																				<MdOutlineKeyboardArrowDown />
-																			</>
-																		)}
-																	</span>
-																</>
-															)}
-														</td>
-													))}
-													{modal &&
-														matchIdx === item.id &&
-														custmr === item?.customer && (
-															<div className="been-shipped">
-																<div className="mt-[30px] text-xs ">
-																	Has this product been shipped ? <br />
-																	<span className="been-shipped-textsm text-green-700 text-sm">
-																		Click YES to add tracking number
-																	</span>
-																</div>
-																<div className="flex flex-row justify-center">
-																	<button
-																		className="mx-2 flex flex-row text-sm items-center hover:text-green-700"
-																		onClick={() => {
-																			setId(item.id)
-																			setShowTrackingInput(true)
-																			scrollToTop()
-																		}}>
-																		<GiCheckMark className="mr-1" />
-																		Yes
-																	</button>
-																	<button
-																		className="mx-2 flex flex-row text-sm items-center hover:text-red-800"
-																		onClick={() => {
-																			setShowTrackingInput(false)
-																			setModal(false)
-																		}}>
-																		<MdClose size={20} className="mr-1" />
-																		No
-																	</button>
-																</div>
-															</div>
-														)}
-												</tr>
+							<span>Data not Found</span>
+						)
+					) : (
+						<>
+							<div className="max-w-[90%] mr-[50px] md:px-[50px] md:max-w-[80%] lg:max-w-[100%] md:overflow-x-auto">
+								<table>
+									<thead>
+										<tr className="table-head-row">
+											{shippingInfo?.shippingHeader?.[0]?.map((head, idx) => (
+												<th key={idx} className="table-head-item">
+													{head}{' '}
+													{head === 'appointment' || head === 'reservation'
+														? ' - date'
+														: ''}
+												</th>
 											))}
-										</tbody>
-									</table>
-								</div>
-							</>
-						)}
-					</main>
-				</div>
-			) : (
+										</tr>
+									</thead>
+									<tbody>
+										{shippingInfo?.shippingData?.map((item, idx) => (
+											<tr className="table-item-row" key={idx}>
+												{Object?.values(item)?.map((itm, index) => (
+													<td
+														className={
+															itm?.length >= 200
+																? 'table-items align-left'
+																: 'table-items'
+														}
+														key={index}
+														onClick={() =>
+															toggleModal(item?.id, item?.customer)
+														}>
+														{typeof itm === 'string'
+															? itm?.length >= 200
+																? readMore && matchIdx === item?.id
+																	? itm
+																	: `${itm.substring(0, 70)}...`
+																: itm
+															: itm}
+														{itm?.length >= 200 && (
+															<>
+																<span onClick={() => toggleRead(item?.id)}>
+																	{readMore && matchIdx === item?.id ? (
+																		<>
+																			<span>Read Less</span>{' '}
+																			<MdOutlineKeyboardArrowUp />
+																		</>
+																	) : (
+																		<>
+																			<span>Read More</span>{' '}
+																			<MdOutlineKeyboardArrowDown />
+																		</>
+																	)}
+																</span>
+															</>
+														)}
+													</td>
+												))}
+												{modal &&
+													matchIdx === item.id &&
+													custmr === item?.customer && (
+														<div className="been-shipped">
+															<div className="mt-[30px] text-xs ">
+																Has this product been shipped ? <br />
+																<span className="been-shipped-textsm text-green-700 text-sm">
+																	Click YES to add tracking number
+																</span>
+															</div>
+															<div className="flex flex-row justify-center">
+																<button
+																	className="mx-2 flex flex-row text-sm items-center hover:text-green-700"
+																	onClick={() => {
+																		setId(item.id)
+																		setShowTrackingInput(true)
+																		scrollToTop()
+																	}}>
+																	<GiCheckMark className="mr-1" />
+																	Yes
+																</button>
+																<button
+																	className="mx-2 flex flex-row text-sm items-center hover:text-red-800"
+																	onClick={() => {
+																		setShowTrackingInput(false)
+																		setModal(false)
+																	}}>
+																	<MdClose size={20} className="mr-1" />
+																	No
+																</button>
+															</div>
+														</div>
+													)}
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+						</>
+					)}
+				</main>
+			</div>
+			{/* ) : (
 				<div className="history-page">
 					<p className="danger">UNAUTHORIZED PATH!</p>
 					<Button to="/" className="btn block total_btn">
 						Back to Home
 					</Button>
 				</div>
-			)}
+			)} */}
 		</div>
 	)
 }
