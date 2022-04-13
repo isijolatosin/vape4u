8b144f7d9e5d3345f933c60c340f1499da6ad752
isijolatosin <isijolatosin2210@gmail.com>
2022-04-13 16:17:49 -0400

    resolving management page

--- src/pages/management.js
+++ src/pages/management.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
 import Heading from '../components/Heading'
 import Inventory from '../components/Inventory'
@@ -6,11 +6,11 @@ import Layout from '../components/shared/Layout'
 import Shippment from '../components/Shippment'
 import { BsFillArrowUpSquareFill } from 'react-icons/bs'
 import AllInventories from '../components/AllInventories'
-import { UserContext } from '../context/user-context'
+// import { UserContext } from '../context/user-context'
 
 function Management() {
 	const [section, setSection] = React.useState('all-inventory')
-	const { user } = useContext(UserContext)
+	// const { user } = useContext(UserContext)
 	// const isSales = localStorage.getItem('isSales')
 
 	const toggleSection = (e) => {
@@ -59,11 +59,11 @@ function Management() {
 							</li>
 						</ul>
 					</div>
-					{user && (
-						<div className="w-[100%]">
-							<SectionComp />
-						</div>
-					)}
+					{/* {user && ( */}
+					<div className="w-[100%]">
+						<SectionComp />
+					</div>
+					{/* )} */}
 					{!section && (
 						<div className="flex flex-col items-center my-20 ">
 							<div className="text-2xl text-stone-600">
