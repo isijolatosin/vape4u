c42faa7df2a0951f7d146d79aeced0319aa26e4d
isijolatosin <isijolatosin2210@gmail.com>
2022-04-16 18:25:56 -0400

    image upload removed from edit form in all inventory page

--- src/components/AllInventories.js
+++ src/components/AllInventories.js
@@ -256,19 +256,19 @@ function AllInventories() {
 								placeholder="Description..."
 								className="rounded mt-1 block lg:w-[50%] mx-auto w-[90%] px-3 py-2 border-none text-sm shadow-xl placeholder-gray-400 focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-200 isabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 outline-0 mb-5 placeholder:font-light placeholder:text-xs text-gray-700 font-light"
 							/>
-							<div className="text-red-800 text-sm mb-2 ">
+							{/* <div className="text-red-800 text-sm mb-2 ">
 								<span>
 									Rename Image and substitute space with a dash ( e.g abc-def )
 								</span>
-							</div>
-							<div className="rounded flex flex-row items-center shadow-xl pl-3 pb-1">
+							</div> */}
+							{/* <div className="rounded flex flex-row items-center shadow-xl pl-3 pb-1">
 								<label className="mr-3 text-gray-500 text-sm">Image</label>
 								<input
 									onChange={uploadFile}
 									type="file"
 									className="block w-full text-sm text-gray-500 file:mr-4 file:py-1 file:px-4 file:rounded file:border-0 file:text-sm file:bg-gray-200 file:text-violet-700 hover:file:bg-violet-100 ease-in duration-300"
 								/>
-							</div>
+							</div> */}
 							<div className="rounded flex flex-row items-center mt-5 shadow-xl p-2">
 								<label className="mr-3 text-gray-500 text-sm">Sales</label>
 								<input
